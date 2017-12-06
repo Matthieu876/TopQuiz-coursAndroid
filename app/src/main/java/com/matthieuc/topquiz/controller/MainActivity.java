deec80ad739cb9e6d964ca0bcf2fb0ca49564a4f
Matthieu <carboni.matthieu@gmail.com>
2017-12-06 16:38:20 +0100

    mise en place de la fonction joueurConnu() pour mémoriser son prénom et son score

--- app/src/main/java/com/matthieuc/topquiz/controller/MainActivity.java
+++ app/src/main/java/com/matthieuc/topquiz/controller/MainActivity.java
@@ -1,6 +1,7 @@
 package com.matthieuc.topquiz.controller;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.Editable;
@@ -21,14 +22,10 @@ public class MainActivity extends AppCompatActivity {
     private User mUser;
 
     public static final int GAME_ACTIVITY_REQUEST_CODE =42;
+    private SharedPreferences mPreferences;
 
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        if(GAME_ACTIVITY_REQUEST_CODE == requestCode && RESULT_OK == resultCode){
-            int score = data.getIntExtra(GameActivity.BUNDLE_EXTRA_SCORE, 0);
-        }
-    }
+    public static final String PREF_KEY_SCORE = "PREF_KEY_SCORE";
+    public static final String PREF_KEY_FIRSTNAME = "PREF_KEY_FIRSTNAME";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,11 +33,14 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         mUser = new User();
+        mPreferences = getPreferences(MODE_PRIVATE);
         mGreetinText = (TextView) findViewById(R.id.activity_main_greeting_txt);
         mNameInput = (EditText) findViewById(R.id.activity_main_name_input);
         mPlayButton = (Button) findViewById(R.id.activity_main_play_btn);
 
         mPlayButton.setEnabled(false);
+
+        joueurConnu();
 
         mNameInput.addTextChangedListener(new TextWatcher() {
             @Override
@@ -66,13 +66,36 @@ public class MainActivity extends AppCompatActivity {
                 String firstname = mNameInput.getText().toString();
                 mUser.setFirstName(firstname);
 
+                mPreferences.edit().putString(PREF_KEY_FIRSTNAME, mUser.getFirstName()).apply();
+
                 //Démarrage de l'activité game
                 Intent gameActivityIntent = new Intent(MainActivity.this, GameActivity.class);
                 startActivityForResult(gameActivityIntent, GAME_ACTIVITY_REQUEST_CODE);
             }
         });
+    }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(GAME_ACTIVITY_REQUEST_CODE == requestCode && RESULT_OK == resultCode){
+            int score = data.getIntExtra(GameActivity.BUNDLE_EXTRA_SCORE, 0);
 
+            mPreferences.edit().putInt(PREF_KEY_SCORE, score).apply();
 
+            joueurConnu();
+        }
+    }
+
+    public void joueurConnu() {
+        String fstname = mPreferences.getString(PREF_KEY_FIRSTNAME, null);
+
+        if (fstname != null) {
+            int points = mPreferences.getInt("PREF_KEY_SCORE", 0);
+            mGreetinText.setText("Bonjour " + fstname + " votre précédent score est de " + points + " points");
+            mNameInput.setText(fstname);
+            mNameInput.setSelection(fstname.length());
+            mPlayButton.setEnabled(true);
+        }
     }
 }
